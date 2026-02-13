002fac84eaa7c3c80cdc33c505606c07362be312
Gamindu Kalmadu <gamindukalmadu8@gmail.com>
2026-02-14 01:17:34 +0530

    mongo db setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Hello World!");
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
@@ -10,3 +11,12 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
     res.send("Welcome to the Nodemon API!.");
 });
+
+mongoose.connect("mongodb+srv://admin_Gamindu:qAOd3twlBHMDfhXx@backenddb.u46kgj6.mongodb.net/?appName=BackendDB")
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch((err) => {
+        console.error("Connection failed:", err);
+    });
+    
