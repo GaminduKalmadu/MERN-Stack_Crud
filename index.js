43314d49931c94aff7e86fbac795a227a78c9d11
Gamindu Kalmadu <gamindukalmadu8@gmail.com>
2026-02-14 16:28:08 +0530

    important details hidding
    database link and port number

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 console.log("Hello World!");
 
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Nodemon API!.");
 });
 
-mongoose.connect("mongodb+srv://admin_Gamindu:qAOd3twlBHMDfhXx@backenddb.u46kgj6.mongodb.net/?appName=BackendDB")
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("Connected to Database");
     })
